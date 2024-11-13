66e7d2668381fd0f07f6b8d4314d551262b2d6f2
Stan Runge <stan@stanrunge.dev>
2024-11-13 15:09:05 +0100

    seed mods

--- packages/frontend/web-laravel/database/seeders/DatabaseSeeder.php
+++ packages/frontend/web-laravel/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,35 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        DB::table('mods')->insert([
+            'name' => 'No Mod',
+            'code' => '',
+        ]);
+
+        DB::table('mods')->insert([
+            'name' => 'Hidden',
+            'code' => 'HD',
+        ]);
+
+        DB::table('mods')->insert([
+            'name' => 'Hard Rock',
+            'code' => 'HR',
+        ]);
+
+        DB::table('mods')->insert([
+            'name' => 'Double Time',
+            'code' => 'DT',
+        ]);
+
+        DB::table('mods')->insert([
+            'name' => 'FreeMod',
+            'code' => 'FM',
+        ]);
+
+        DB::table('mods')->insert([
+            'name' => 'Tiebreaker',
+            'code' => 'TB',
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
