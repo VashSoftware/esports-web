8e42687288df1f41db8fa31a7e58cbbcd9bc3150
Stan Runge <stan@stanrunge.dev>
2024-11-22 22:10:27 +0100

    style: mappools page

--- packages/web/resources/js/Pages/MapPools/Index.svelte
+++ packages/web/resources/js/Pages/MapPools/Index.svelte
@@ -6,26 +6,33 @@
 </script>
 
 <Layout>
-    <Link href="/map_pools/create">
-        <button>Create</button>
-    </Link>
+    <div class="my-5 flex items-center justify-between">
+        <div></div>
+        <h1 class="text-center text-2xl">Map Pools</h1>
 
-    <table class="table-auto">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Created At</th>
-                <th>Mods</th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each mapPools as pool}
-                <tr onclick={router.visit(`/map_pools/${pool.id}`)}>
-                    <td>{pool.name}</td>
-                    <td>{pool.description}</td>
-                    <td>{pool.created_at}</td>
+        <Link href="/map_pools/create">
+            <button class="mx-16 rounded bg-blue-500 px-4 py-2">Create</button>
+        </Link>
+    </div>
+
+    <div class="my-16 flex flex-col content-center">
+        <table class="table-auto">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Created At</th>
+                    <th>Mods</th>
                 </tr>
-            {/each}
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                {#each mapPools as pool}
+                    <tr onclick={router.visit(`/map_pools/${pool.id}`)}>
+                        <td>{pool.name}</td>
+                        <td>{pool.description}</td>
+                        <td>{pool.created_at}</td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+    </div>
 </Layout>
