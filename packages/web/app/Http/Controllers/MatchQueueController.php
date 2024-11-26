1ccf74381dc3503eb09f1b7321c23aa30cbf0495
Stan Runge <stan@stanrunge.dev>
2024-11-26 19:59:09 +0100

    fix: match queue

--- packages/web/app/Http/Controllers/MatchQueueController.php
+++ packages/web/app/Http/Controllers/MatchQueueController.php
@@ -3,20 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redis;
 
 class MatchQueueController extends Controller
 {
     public function join(Request $request)
     {
-        Redis::lpush('match_queue', $request->input('team_id'));
+        $isMember = Redis::sismember('match_queue', $request->user()->profile->personalTeam()->id);
+        Log::debug($isMember);
+
+        if (!$isMember) {
+            Redis::sadd('match_queue', $request->user()->profile->personalTeam()->id);
+        }
 
         return back(status: 303);
     }
 
     public function leave(Request $request)
     {
-        Redis::rpop('match_queue', $request->input('team_id'));
+        Redis::srem('match_queue', $request->input('team_id'));
 
         return back(status: 303);
     }
