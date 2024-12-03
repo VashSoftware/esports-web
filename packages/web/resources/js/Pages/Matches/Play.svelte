3c8d23df68fea8ae2d6ba4abc2157deec98db958
Stan Runge <stan@stanrunge.dev>
2024-12-03 20:34:51 +0100

    feat: share match button

--- packages/web/resources/js/Pages/Matches/Play.svelte
+++ packages/web/resources/js/Pages/Matches/Play.svelte
@@ -13,15 +13,23 @@
     }
 
     let canPickMaps = $state(false)
+    let shareModalShown = $state(false)
 </script>
 
 <Layout>
-    <h1 class="my-4 text-center text-2xl font-bold">Match Play</h1>
+    <div class="flex justify-around">
+        <div></div>
+        <h1 class="my-4 text-center text-2xl font-bold">Match Play</h1>
+        <button onclick={() => (shareModalShown = true)} class="bg-gray-500 p-2">Share</button>
+    </div>
 
-    <h2>Maps</h2>
+    <h2 class="text-center text-xl font-bold">Maps</h2>
     {#each match.match_maps as map}
         <div class="flex">
-            <div>Scores Team 1</div>
+            <div>
+                Scores Team 1
+                {#each map.scores as score}{/each}
+            </div>
             <div>
                 <img src="" alt="" />
                 <div>
@@ -29,7 +37,10 @@
                         .difficulty_name}]
                 </div>
             </div>
-            <div>Scores Team 2</div>
+            <div>
+                Scores Team 2
+                {#each map.scores as score}{/each}
+            </div>
         </div>
     {/each}
 </Layout>
@@ -43,5 +54,21 @@
                 {/each}
             {/each}
         </form>
+    </div>
+{/if}
+
+{#if shareModalShown}
+    <div class="fixed inset-0 z-10 bg-black opacity-50"></div>
+    <div class="fixed inset-0 z-20 flex items-center justify-center" onclick={() => (shareModalShown = false)}>
+        <div class="relative w-1/3 rounded-xl bg-white p-8 text-center shadow-xl" onclick={(e) => e.stopPropagation()}>
+            <h3 class="text-xl font-bold">Share Match</h3>
+            <p>Your share URL is:</p>
+            <input
+                type="text"
+                class="my-8 w-full text-center"
+                value="https://esports.vash.software/matches/{match.id}"
+                readonly
+            />
+        </div>
     </div>
 {/if}
