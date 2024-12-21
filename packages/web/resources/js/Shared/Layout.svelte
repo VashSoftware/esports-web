394377138c82f1a0fba04c4d048afd2bb67edecb
Stan Runge <stan@stanrunge.dev>
2024-12-21 18:57:30 +0100

    fix: temp remove events link

--- packages/web/resources/js/Shared/Layout.svelte
+++ packages/web/resources/js/Shared/Layout.svelte
@@ -89,14 +89,15 @@
                             Matches
                         </Link>
                     </li>
-                    <li>
+                    <!--                    <li>
                         <Link
                             href="/events"
                             class="block rounded px-4 py-2 transition-colors hover:bg-primary hover:bg-opacity-75"
                         >
                             Events
                         </Link>
-                    </li>
+                   </li>
+   -->
                     <li>
                         <Link
                             href="/leaderboard"
