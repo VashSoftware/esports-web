2a92cfc58e23e8ea8240720c2c06bff01722440c
Stan Runge <stan@stanrunge.dev>
2025-02-04 04:18:32 +0100

    add express server and settimeout matchmaking

--- packages/matchmaking/src/index.ts
+++ packages/matchmaking/src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+async function runMatchmaking() {
+  console.log("Running matchmaking");
+}
+
+setInterval(() => {
+  runMatchmaking();
+}, 10000);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Express server is up and running!");
+});
+
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
+});
